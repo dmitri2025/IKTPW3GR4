d4d87dcf2d599258490f5867408739f912405c3c
Opilane TTHK <opilane@tthk.ee>
2025-12-11 11:05:55 +0200

    eeeeeez
    d

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+                int i, j, rows;
+                Console.Write("\n\n");
+                Console.Write("Tärnidest tehtud kolmnurk e poolik püramiid:\n");
+                Console.Write("--------------------------------------------------------");           
+                Console.Write("\n\n");
+
+                Console.Write("Sisesta ridade arv: ");
+                rows = Convert.ToInt32(Console.ReadLine());
+
+                for (i = 1; i <= rows; i++)
+                {
+                    for (j = 1; j <= i; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.Write("\n");
+                }
+            }
+        }
+    }
+
+
+
---
+++ ForLoopMulti/ForLoopMulti/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ForLoopMulti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Korrutustabel");
+
+            int j, i, n;
+
+            Console.WriteLine("Sisesta tabelisse number, millest soovid alustada");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Korrutustabel ühest kuni {0}", n);
+            Console.WriteLine("\n");
+
+            for (i = 1; i < 10; i++) 
+            {
+                for (j = 1; j <= n; j++)
+                {
+                    if (j <= n - 1)
+                    {
+                        Console.Write("{0}x{1} = {2},", j, i, i * j);
+                    }
+                    else 
+                    {
+                    Console.WriteLine("{0}x{1} = {2} ", j, i, i * j);
+                    }
+                }
+                Console.WriteLine("\n");
+            }
+
+        }
+    }
+}
